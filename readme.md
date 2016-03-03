184660204d13663449b4e69c3d4164d36874f9d0
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-03 10:40:41 -0500

    clicking on the board populates Xs and Ox

--- readme.md
+++ readme.md
@@ -2,16 +2,27 @@ Identify the Problem:
 
 1. What elements should we create variables for?
 
+a variable to keep track of moves
+
 2. What events should we listen for?
+
+clicking on the grid squares
 
 3. What elements need to be created?
 
+divs for a 3x3 grid
+
 4. How is user input handled?
+
+clicks / event listeners
 
 5. What do we need to keep track of?
 
+the game moves
+
 6. What can be calculated from the things we need to keep track of?
 
+who's turn it is
 
 
 /////////
@@ -51,6 +62,11 @@ Bronze:
 
 When the user clicks, an X appears on the grid
 When the user clicks again, an O appears on the grid
+If there are 3 Xs or Os in a row or diagonally, X or O respectively wins
 
+Silver:
+
+There is a reset button that will clear the board and start again
+Once a cell is marked, it's background color changes using js to change the class
 
 ////
---
+++ script.js
@@ -0,0 +1,93 @@
+var game = {
+  moves: ["X", "O", "X", "O", "X", "O", "X", "O", "X"],
+  numMoves: 0,
+  tiles: []
+};
+
+$('document').ready(function(){
+
+$("#tile1").on("click", function(){
+    $(this).removeClass("open").addClass("taken");
+    $(this).html(game.moves.shift());
+    console.log("This: " + this);
+    console.log(game.moves.shift);
+    game.numMoves++;
+    console.log(game.numMoves);
+  }
+);
+
+$("#tile2").on("click", function(){
+  $(this).removeClass("open").addClass("taken");
+  $(this).html(game.moves.shift());
+  console.log(this);
+}
+);
+
+$("#tile3").on("click", function(){
+  $(this).removeClass("open").addClass("taken");
+  $(this).html(game.moves.shift());
+  console.log(this);
+}
+);
+
+$("#tile4").on("click", function(){
+  $(this).removeClass("open").addClass("taken");
+  $(this).html(game.moves.shift());
+  console.log(this);
+}
+);
+
+$("#tile5").on("click", function(){
+  $(this).removeClass("open").addClass("taken");
+  $(this).html(game.moves.shift());
+  console.log(this);
+}
+);
+
+$("#tile6").on("click", function(){
+  $(this).removeClass("open").addClass("taken");
+  $(this).html(game.moves.shift());
+  console.log(this);
+}
+);
+
+$("#tile7").on("click", function(){
+  $(this).removeClass("open").addClass("taken");
+  $(this).html(game.moves.shift());
+  console.log(this);
+}
+);
+
+$("#tile8").on("click", function(){
+  $(this).removeClass("open").addClass("taken");
+  $(this).html(game.moves.shift());
+  console.log(this);
+}
+);
+
+$("#tile9").on("click", function(){
+  $(this).removeClass("open").addClass("taken");
+  $(this).html(game.moves.shift());
+  console.log(this);
+}
+);
+
+
+
+});
+
+// var tile = $('<div></div>')
+// .hide()
+// .append($('<table></table>')
+//     .attr({ cellSpacing : 0 })
+//     .addClass("text")
+
+// this makes sure the completed grid is removed at the end of a game
+// function buildGrid(){
+//     var parentDiv = $("#grid");
+//     for (var i = 0; i <= 9; i++){
+//       var tile = $("#grid").append($("<div></div>")).addClass("open");
+//       tile.id = "tile" + i;
+//       $("#grid").append(tile);
+//     }
+//   }
