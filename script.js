12387293c37dda126569e26ac738ba88cb3e64fa
Clarissa Stark <clarissa.stark@gmail.com>
2016-03-03 16:15:34 -0500

    cleaning up unused code

--- script.js
+++ script.js
@@ -3,9 +3,6 @@ var game = {
   movesX: ["X", "O", "X", "O", "X", "O", "X", "O", "X"],
   movesO: ["O", "X", "O", "X", "O", "X", "O", "X", "O"],
   numMoves: 0,
-  rows: [[$("#tile1").html(),$("#tile2").html(),$("#tile3").html()],[$("#tile4").html(),$("#tile5").html(),$("#tile6").html()],[$("#tile7").html(),$("#tile8").html(),$("#tile9").html()]],
-  cols: [[$("#tile1"),$("#tile4"),$("#tile7")],[$("#tile2"),$("#tile5"),$("#tile8")],[$("#tile3"),$("#tile6"),$("#tile9")]],
-  diags: [[$("#tile1"),$("#tile5"),$("#tile9")],[$("#tile3"),$("#tile5"),$("#tile7")]],
   tile1: $("#tile1"),
   tile2: $("#tile2"),
   tile3: $("#tile3"),
@@ -54,9 +51,6 @@ function clearBoard(){
   }
 }
 
-
-// ((game.rows[0][0].html() !== "") (game.rows[0][1].html() !== "") && (game.rows[0][2].html() !== "") && (game.rows[0][0].html() === game.rows[0][1].html()) && (game.rows[0][1].html() === game.rows[0][2].html()))
-
 function winsRow(){
   if((game.tile1.html() !== "") && (game.tile2.html() !== "") && (game.tile3.html() !== "") && game.tile1.html() == game.tile2.html() && game.tile2.html() == game.tile3.html()){
     alert(game.tile1.html() + " is the winner!");
@@ -92,18 +86,6 @@ function winsDiag(){
     return game.tile3.html();
   }
 }
-// if(game.rows.forEach(allValuesSame) == true){
-//   console.log("There is a winner!");
-// }
-// if((game.rows[0][0].html() === game.rows[0][1].html()) && (game.rows[0][1].html() === game.rows[0][2].html()) !== ""){
-//   console.log(game.rows[0][0].html() + " is the winner!");
-// }else if((game.tile4.html() !== null) && (game.tile5.html() !== null) && (game.tile6.html() !== null) && (game.tile4.html() === game.tile5.html()) && (game.tile5.html() === game.tile6.html())){
-//   console.log(game.rows[1][0].html() + " is the winner!");
-// }else if((game.tile8 !== null) && (game.tile9 !== null) && (game.tile7.html() === game.tile8.html()) && (game.tile8.html() === game.tile9.html()) && (game.tile7.html() !== null)){
-//   console.log(game.rows[2][0].html() + " is the winner!");
-// }
-
-
 
 function loopRows(){
   for(var i = 0; i < game.rows.length; i++){
@@ -117,10 +99,3 @@ function loopRows(){
     return true;
   }
 }
-
-
-
-
-// add an option to play as Xs or Os
-// add players
-// add class color change for taken
